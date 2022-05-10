e39848d3a8a39ef94dacd9a06ff7fc8006cdcc4f
ioana-anton <94392890+ioana-anton@users.noreply.github.com>
2022-05-10 03:30:06 +0300

    Header + About you + Portofolio

---
+++ script/script.js
@@ -0,0 +1,12 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar'); 
+//the query selector returns the first element that matches the CSS selector
+window.onscroll = function() { //Fires up when the scroll event happens
+    var top = window.scrollY; //returns the number of pixels that the document is scrolled vertically
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
